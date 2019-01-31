b2551b29536ab4688622bbc07ab2d903a951c58e
jgryn <jonathangryn@jonathans-mbp.ieee.org>
2019-01-30 16:38:05 -0800

    test

--- script.js
+++ script.js
@@ -35,6 +35,7 @@ console.log(mark.name + ' ' + mark.lastName);
 
 
 // Object.create
+/*
 var personProto = {
     calculateAge: function() {
         console.log(2016 - this.yearOfBirth);
@@ -52,3 +53,66 @@ var jane = Object.create(personProto,
     yearOfBirth: { value: 1969 },
     job: { value: 'designer' }
 });
+*/
+
+
+// Primitives vs objects
+
+// Primitives
+var a = 23;
+var b = a;
+a = 46;
+console.log(a);
+console.log(b);
+
+
+// Objects
+var obj1 = {
+    name: 'John',
+    age: 26
+};
+var obj2 = obj1;
+obj1.age = 30;
+console.log(obj1.age);
+console.log(obj2.age);
+
+// Functions
+var age = 27;
+var obj = {
+    age: 27,
+    name: 'Jonas',
+    city: 'Lisbon'
+};
+
+function change(b) {
+    b.age = 30;
+    b.city = 'San Francisco';
+}
+
+change(obj);
+
+console.log(obj.age);
+console.log(obj.city);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
