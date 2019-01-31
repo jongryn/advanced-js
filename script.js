5dffa5cd2790524a9b4a7e94f38a24f353fa56bb
jgryn <jonathangryn@jonathans-mbp.ieee.org>
2019-01-30 16:19:46 -0800

    test

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // Function constructor
-
+/*
 var john = {
     name: 'John',
     yearOfBirth: 1990,
@@ -29,3 +29,26 @@ mark.calculateAge();
 console.log(john.name + ' ' + john.lastName);
 console.log(jane.name + ' ' + jane.lastName);
 console.log(mark.name + ' ' + mark.lastName);
+*/
+
+
+
+
+// Object.create
+var personProto = {
+    calculateAge: function() {
+        console.log(2016 - this.yearOfBirth);
+    }
+};
+
+var john = Object.create(personProto);
+john.name = 'John';
+john.yearOfBirth = 1990;
+john.job = 'teacher';
+
+var jane = Object.create(personProto,  
+{
+    name: { value: 'Jane' },
+    yearOfBirth: { value: 1969 },
+    job: { value: 'designer' }
+});
