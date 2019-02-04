70d0290755304e63a0d4b0e7e0d6f6fc80f0280b
jgryn <jonathangryn@jonathans-mbp.ieee.org>
2019-02-04 11:55:44 -0800

    test

--- script.js
+++ script.js
@@ -182,7 +182,7 @@ function game() {
 }
 game();
 */
-
+/*
 (function () {
     var score = Math.random() * 10;
     console.log(score >= 5);
@@ -194,4 +194,65 @@ game();
     var score = Math.random() * 10;
     console.log(score >= 5 - goodluck);
 })(5);
+*/
 
+
+
+
+//////////////////////////////
+// Lecture: Closures
+
+function retirement(retirementAge) {
+    var a = ' years left until retirement.';
+    return function(yearOfBirth) {
+        var age = 2016 - yearOfBirth;
+        console.log((retirementAge - age) + a);
+    }
+}
+
+var retirementUS = retirement(66);
+var retirementGermany = retirement(65);
+var retirementIceland = retirement(67);
+
+retirementGermany(1990);
+retirementUS(1990);
+retirementIceland(1990);
+
+// retirement(66)(1990);
+
+/*
+function interviewQuestion(job) {
+    if (job === 'designer') {
+        return function(name) {
+            console.log(name + ', can you please explain what UX design is?');
+        }
+    } else if (job === 'teacher') {
+        return function(name) {
+            console.log('What subject do you teach, ' + name + '?');
+        }
+    } else {
+        return function(name) {
+            console.log('Hello ' + name + ', what do you do?');
+        }
+    }
+}
+*/
+
+function interviewQuestion(job) {
+    return function(name) {
+        if (job === 'designer') {
+            console.log(name + ', can you please explain what UX design is?');
+        } else if (job === 'teacher') {
+            console.log('What subject do you teach, ' + name + '?');
+        } else {
+            console.log('Hello ' + name + ', what do you do?');
+        }
+    }
+}
+
+interviewQuestion('teacher')('John');
+interviewQuestion('designer')('John');
+interviewQuestion('designer')('Jane');
+interviewQuestion('designer')('Jane');
+interviewQuestion('designer')('Mark');
+interviewQuestion('other')('Mike');
